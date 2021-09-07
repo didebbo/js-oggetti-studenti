77735f33b6722f2feb94f0efe0b920626aca9f33
didebbo <gianluca.napoletano@gmail.com>
2021-09-07 15:16:25 +0200

    stampo l'oggetto studente

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-alert("init");
+// Globali
+const studente = {
+    nome: "Gianluca",
+    cognome: "Napoletano",
+    eta: 28
+}
+
+// Funzioni
+const main = () => {
+    let log = "Studente:\n";
+    for (const key in studente) {
+        log += key + " => " + studente[key] + "\n";
+    }
+    alert(log);
+}
+
+// Main
+main();
