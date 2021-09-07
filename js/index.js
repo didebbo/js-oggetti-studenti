de6fd206d0cb83765fe1769eca9d80d6374191fc
didebbo <gianluca.napoletano@gmail.com>
2021-09-07 15:38:17 +0200

    esercizio1 + esercizio2

--- js/index.js
+++ js/index.js
@@ -1,17 +1,68 @@
-// Globali
-const studente = {
-    nome: "Gianluca",
-    cognome: "Napoletano",
-    eta: 28
-}
+
 
 // Funzioni
-const main = () => {
+const scegliEsercizio = (arg1) => {
+    switch (arg1) {
+        case 1:
+            esercizio1();
+            break;
+
+        case 2:
+            esercizio2();
+            break;
+
+        default:
+            main();
+            break;
+    }
+}
+
+// Esercizio 1
+const esercizio1 = () => {
+    const studente = {
+        nome: "Gianluca",
+        cognome: "Napoletano",
+        eta: 28
+    }
+
     let log = "Studente:\n";
     for (const key in studente) {
         log += key + " => " + studente[key] + "\n";
     }
     alert(log);
+}
+
+// Esercizio 2
+const esercizio2 = () => {
+    const studenti = [
+        {
+            nome: "Samuele",
+            cognome: "Madrigali",
+            eta: 30
+        },
+        {
+            nome: "Alessandro",
+            cognome: "Lodigiani",
+            eta: 30
+        },
+        {
+            nome: "Fabrizio",
+            cognome: "Mastrobattista",
+            eta: 30
+        }
+    ];
+
+    let log = "Studenti:\n";
+
+    studenti.forEach((studente, index) => {
+        log += (index + 1) + "° studente: " + studente["nome"] + " " + studente["cognome"] + "\n";
+    });
+    alert(log);
+
+}
+
+const main = () => {
+    scegliEsercizio(parseInt(prompt("Scegli numero esercizio:\nEs. 1 o 2")));
 }
 
 // Main
