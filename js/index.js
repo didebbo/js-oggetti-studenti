571b6c045b15c1a7b1b830a773d910f28c4505ca
didebbo <gianluca.napoletano@gmail.com>
2021-09-07 15:50:08 +0200

    esercizio2 + inserimento nuovo studente

--- js/index.js
+++ js/index.js
@@ -36,6 +36,11 @@ const esercizio1 = () => {
 const esercizio2 = () => {
     const studenti = [
         {
+            nome: "Gianluca",
+            cognome: "Napoletano",
+            eta: 28
+        },
+        {
             nome: "Samuele",
             cognome: "Madrigali",
             eta: 30
@@ -52,13 +57,27 @@ const esercizio2 = () => {
         }
     ];
 
-    let log = "Studenti:\n";
+    const stampaStudenti = (studenti) => {
+        let log = "Studenti:\n";
 
-    studenti.forEach((studente, index) => {
-        log += (index + 1) + "° studente: " + studente["nome"] + " " + studente["cognome"] + "\n";
-    });
-    alert(log);
+        studenti.forEach((studente, index) => {
+            log += (index + 1) + "° studente: " + studente["nome"] + " " + studente["cognome"] + "\n";
+        });
+        alert(log);
+    }
 
+    stampaStudenti(studenti);
+
+    // Nuovo studente
+    alert("Adesso inserisci un nuovo studente alla classe.")
+    let studente = {
+        nome: prompt("Nome Studente:"),
+        cognome: prompt("Cognome Studente")
+    }
+    do studente["eta"] = parseInt(prompt("Età studente:"));
+    while (isNaN(studente["eta"]));
+    studenti.push(studente);
+    stampaStudenti(studenti);
 }
 
 const main = () => {
